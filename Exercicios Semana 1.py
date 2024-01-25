7ebaabfd5fc7bc0372858320b0306ad3bd83b990
Martina T <56701114+Tina016@users.noreply.github.com>
2024-01-25 16:57:08 -0300

    Add files via upload

---
+++ Exercicios Semana 1.py
@@ -0,0 +1,6 @@
+rua = input("Insira sua rua:")
+bairro = input("Insira seu bairro:")
+cidade = input("Insira sua cidade:")
+cep = input("Insira seu cep:")
+
+print("\n","Rua:", rua, "\n", "Bairro:", bairro, "\n", "Cidade:", cidade, "\n", "Cep:", cep)
